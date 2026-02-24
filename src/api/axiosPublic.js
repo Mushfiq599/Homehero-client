7f4906542920e2e7d9c9fe56577418fb657d2d6d
Mushfiq599 <mellowm678@gmail.com>
2026-02-24 23:54:31 +0600

    server url added

--- src/api/axiosPublic.js
+++ src/api/axiosPublic.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const baseURL = import.meta.env.VITE_API_URL || "https://homehero-server.vercel.app";
 const api = axios.create({
   baseURL,
 });
